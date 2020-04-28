262a0315a19094bf1582583d50846f6d9f74fdd2
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-04-28 02:30:41 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("css"));
+app.use(express.static("public"));
+
+var firstImgs = ["sand", "mountains", "landscapes", "stuff", "blocks", "chairs", "interior", "crocodile", "alligator", "chevrolet", "movie", "theatre"];
+
+
+const request = require('request');
+
+//routes
+app.get("/", async function(req, res){
+    
+ let firstImg = Math.floor(Math.random() * 11);   
+ let parsedData = await getImages(firstImgs[firstImg]);
+ 
+ console.dir("parsedData: " + parsedData); //displays content of the object
+    
+ res.render("index", {"images":parsedData});
+
+            
+}); //root route
+
+
+app.get("/results", async function(req, res){
+    let keyword = req.query.keyword; //gets the value that the user typed in the form using the GET method
+    
+    let orie = req.query.orientation;
+    
+    let parsedData = await getImages(keyword, orie);
+
+    res.render("results", {"images":parsedData});
+    
+});//results route
+
+
+//Returns all data from the Pixabay API as JSON format
+function getImages(keyword, orientation){
+    
+    
+    return new Promise( function(resolve, reject){
+        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=' + keyword
+        + '&orientation=' + orientation,
+                 function (error, response, body) {
+    
+            if (!error && response.statusCode == 200  ) { //no issues in the request
+                
+                 let parsedData = JSON.parse(body); //converts string to JSON
+                 
+                 resolve(parsedData);
+                
+                
+            } else {
+                reject(error);
+                console.log(response.statusCode);
+                console.log(error);
+            }
+    
+          });//request
+   
+    });
+    
+}
+
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+})
